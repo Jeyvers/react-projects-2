b224af4f5fe38a006d4d4c23ed35c676e2c33d65
Jeyi Adole <erthnice@gmail.com>
2022-03-03 02:52:56 +0100

    Modified setup

--- 01-todo-list/src/App.js
+++ 01-todo-list/src/App.js
@@ -3,12 +3,40 @@ import './index.css';
 import Form from './form.jsx';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = (index) =>
+    setTodos(
+      todos.map((todo, key) =>
+        key === index
+          ? {
+              ...todo,
+              complete: !todo.complete,
+            }
+          : todo
+      )
+    );
   return (
-    <div className='App'>
-      <Form />
-    </div>
+    <>
+      <div className='App'>
+        <Form
+          onSubmit={(text) => setTodos([{ text, complete: false }, ...todos])}
+        />
+      </div>
+      <div>
+        {todos.map(({ text, complete }, index) => (
+          <div
+            key={text}
+            onClick={() => toggleComplete(index)}
+            style={{ textDecoration: complete ? 'line-through' : '' }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}>Reset</button>
+    </>
   );
 };
 
 export default App;
-// Can't push today, git not responding.
