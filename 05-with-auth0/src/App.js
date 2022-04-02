7cf37f6cadcd18e945dfa3ba30d2dce5c0a6a966
Jeyi Adole <erthnice@gmail.com>
2022-04-02 13:52:41 +0100

    paused project

---
+++ 05-with-auth0/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Hi Auth0</div>;
+}
+
+export default App;
---
+++ 05-with-auth0/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Auth0Provider } from '@auth';
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
